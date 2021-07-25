0527d01a44196494c2ee4674b5ab3fe356c7a547
PirateYT <87933270+PirateYT@users.noreply.github.com>
2021-07-25 16:48:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+
+
+function getDetails(place){
+
+
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
+.then(res => res.json())
+.then(abc=> document.getElementById('name').innerHTML = abc.name)
+
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
+.then(res => res.json())
+.then(abc=> document.getElementById('lat').innerHTML = abc.coord.lat)
+
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
+.then(res => res.json())
+.then(abc=> document.getElementById('lon').innerHTML = abc.coord.lon)
+
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
+.then(res => res.json())
+.then(abc=> document.getElementById('country').innerHTML = abc.sys.country)
+
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
+.then(res => res.json())
+.then(abc=> document.getElementById('temp').innerHTML = parseInt(abc.main.temp-273.15)+"°C")
+
+
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
+.then(res => res.json())
+.then(abc=> document.getElementById('speed').innerHTML = abc.wind.speed+"Km/h")
+
+}
+
+function callme(){
+    var myplace = document.getElementById('place').value;
+    getDetails(myplace)
+
+
+}
+
+
+
+
